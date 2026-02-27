c067cb6536ffef1eb206d57e2c8b42b5b7321680
neerajKashyap-cmd <dabarkashyap31@gmail.com>
2026-02-28 02:58:30 +0530

    fixed production cookie

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -55,10 +55,10 @@ export const signIn=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
-            maxAge:7*24*60*60*1000,
-            httpOnly:true
+              secure: true,
+              sameSite: "none",
+              maxAge: 7 * 24 * 60 * 60 * 1000,
+             httpOnly: true
         })
   
         return res.status(200).json(user)
@@ -142,10 +142,10 @@ export const googleAuth=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
-            maxAge:7*24*60*60*1000,
-            httpOnly:true
+             secure: true,
+             sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true
         })
   
         return res.status(200).json(user)
